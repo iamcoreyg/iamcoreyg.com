6a5099a0a95df46b1b810653c828e6fc23855c84
Corey Griffin <iamcoreyg@gmail.com>
2024-08-24 19:50:11 -0700

    dark

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
+  // credentials are optional
+  var spotifyApi = new SpotifyWebApi({
+    clientId: '',
+    clientSecret: '',
+    redirectUri: 'http://www.iamcoreyg.com/callback'
+  });
+
+  console.log('spotifyApi', spotifyApi);
+
+  spotifyApi.getUser('iamcoreyg')
+  .then(function(data) {
+    console.log('Some information about this user', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
